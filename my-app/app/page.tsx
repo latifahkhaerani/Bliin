88e485fa8f197580fba57e7ade6a6eed140820d6
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-06 18:04:13 +0700

    style: add style on home page

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,20 @@
-
-
 export default function Home() {
   return (
-   <>
-   </>
+    <>
+      <div className="bg-lightPink h-70  pt-20 flex-1">
+        <div className="px-5 h-70  bg-yellow rounded-3xl">
+          <div className=" object-bottom w-1/4">
+            <h1 className="text-5xl font-extrabold text-white ">
+              Get Yours Now!
+            </h1>
+            <button className="bg-primary rounded-3xl w-fit py-1 px-3">
+              <p className="text-white font-medium ">Click to buy</p>
+            </button>
+          </div>
+
+          <img src="" alt="" />
+        </div>
+      </div>
+    </>
   );
 }

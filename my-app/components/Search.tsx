0f7ef9f9291485f27718e62be3dbe99668156546
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-07 15:33:56 +0700

    style: add search comp

--- my-app/components/Search.tsx
+++ my-app/components/Search.tsx
@@ -1,3 +1,12 @@
-export default function Search() {
-  return <></>;
+import { Search } from "lucide-react";
+
+export default function SearchBar() {
+  return (
+    <>
+      <div className="flex">
+        <Search className="text-abu mr-3" />
+        <input type="text" name="Search" id="" placeholder="Search our store" />
+      </div>
+    </>
+  );
 }

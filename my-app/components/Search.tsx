4ce9c506a2356e3e134dc55705cb0d0b2bd7cbf3
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-07 17:40:15 +0700

    style: fix component search

--- my-app/components/Search.tsx
+++ my-app/components/Search.tsx
@@ -2,11 +2,15 @@ import { Search } from "lucide-react";
 
 export default function SearchBar() {
   return (
-    <>
-      <div className="flex">
-        <Search className="text-abu mr-3" />
-        <input type="text" name="Search" id="" placeholder="Search our store" />
-      </div>
-    </>
+    <div className="mx-auto mt-10 flex h-14 w-full max-w-xl items-center rounded-xl border border-gray-300 px-5">
+      <Search className="mr-3 h-5 w-5 text-gray-400" />
+
+      <input
+        type="text"
+        name="search"
+        placeholder="Search our store"
+        className="w-full bg-transparent text-lg text-gray-600 outline-none placeholder:text-gray-400"
+      />
+    </div>
   );
 }

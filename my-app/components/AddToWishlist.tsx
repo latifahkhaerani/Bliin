7359158a9efdd49edf2b03deb3d13a327746fe5b
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-07 15:37:12 +0700

    style: add comp add to wishtlist

--- my-app/components/AddToWishlist.tsx
+++ my-app/components/AddToWishlist.tsx
@@ -1,3 +1,11 @@
+import { Heart } from "lucide-react";
+
 export default function AddToWishlist() {
-  return <></>;
+  return (
+    <>
+      <button className="mt-3 flex w-full justify-center text-lg text-primary">
+        <Heart /> &nbsp; Add to Wishlist
+      </button>
+    </>
+  );
 }

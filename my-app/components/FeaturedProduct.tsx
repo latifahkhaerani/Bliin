5c3560fe70adf758c625f468c29dae7779abdc44
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-07 16:11:14 +0700

    style: add comp featuredProduct

--- my-app/components/FeaturedProduct.tsx
+++ my-app/components/FeaturedProduct.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import ListProduct from "./ListProduct";
+
 export default function FeaturedProduct() {
-  return <></>;
+  return (
+    <>
+      {/* TITLE */}
+      <section className="mt-6 text-center">
+        <h2 className="text-5xl font-extrabold text-primary mb-5">
+          SHOP LUCKY BAGS
+        </h2>
+        <Link
+          href="/products"
+          className=" text-lg text-primary underline underline-offset-4 hover:opacity-70"
+        >
+          Shop All Products
+        </Link>
+      </section>
+      <ListProduct />
+    </>
+  );
 }

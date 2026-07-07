8a7ee7a9abf998b8741d86e919dad042ac4322d3
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-07 17:43:20 +0700

    refactor: move code to components

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,20 +1,15 @@
+"use server";
+
+import Banner from "@/components/Banner";
+import DetailInfo from "@/components/DetailInfo";
+import FeaturedProduct from "@/components/FeaturedProduct";
+
 export default function Home() {
   return (
     <>
-      <div className="bg-lightPink h-70  pt-20 flex-1">
-        <div className="px-5 h-70  bg-yellow rounded-3xl">
-          <div className=" object-bottom w-1/4">
-            <h1 className="text-5xl font-extrabold text-white ">
-              Get Yours Now!
-            </h1>
-            <button className="bg-primary rounded-3xl w-fit py-1 px-3">
-              <p className="text-white font-medium ">Click to buy</p>
-            </button>
-          </div>
-
-          <img src="" alt="" />
-        </div>
-      </div>
+      <Banner />
+      <DetailInfo />
+      <FeaturedProduct />
     </>
   );
 }

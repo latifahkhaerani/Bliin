e6028d8ac28816ce48f20741889a76279e0b511f
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-08 15:51:14 +0700

    style: add comp addRemoveWishlist on the homePage

---
+++ my-app/components/AddRemoveWishlist.tsx
@@ -0,0 +1,12 @@
+import { Heart } from "lucide-react";
+
+export default function AddRemoveWishlist() {
+  return (
+    <button
+      type="button"
+      className="absolute right-4 top-4 flex h-10 w-10 items-center justify-center rounded-full bg-white/80 transition hover:scale-110 hover:bg-white"
+    >
+      <Heart className="h-6 w-6 text-primary" />
+    </button>
+  );
+}

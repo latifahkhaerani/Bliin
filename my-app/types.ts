8df9f8c91fcbe9e85fe2d3751a0e8e6decb9da2f
Latifahtul Khaerani A <khaeranifah@gmail.com>
2026-07-08 15:53:34 +0700

    chore: add TypeScript types for products

---
+++ my-app/types.ts
@@ -0,0 +1,35 @@
+export type ProductType = {
+    _id: number;
+  //   name: string;
+  //   slug: string;
+  //   description: string;
+  //   excerpt: string;
+  //   price: number;
+  //   tags: string[];
+  //   thumbnail: string;
+  //   images: string[];
+  //   createdAt: string;
+  //   updatedAt: string;
+  name: string;
+  slug: string;
+  description: string;
+  excerpt: string;
+  price: number;
+  originalPrice?: number;
+
+  thumbnail: string;
+  images: string[];
+
+  rating: number;
+  reviews: number;
+
+  stock: number;
+  freeShipping: boolean;
+  rewards: number;
+
+  features: string[];
+  tags: string[];
+
+  createdAt: Date;
+  updatedAt: Date;
+};
